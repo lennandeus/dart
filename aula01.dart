6042608bb5e9e7298232950b8c0c315670a8dd01
JUAN VITOR TASCA DE ANDRADE <juan.4910@aluno.pr.senac.br>
2024-05-02 09:43:50 -0300

    first commit

---
+++ aula01.dart
@@ -0,0 +1,14 @@
+void main (){
+  int nota1 = 2;
+  int nota2 = 2;
+  int nota3 = 2;
+  int nota4 = 2;
+  double media = (nota1+nota2+nota3+nota4/4);
+  if (media >=7){
+    print("Você está na média");
+  }else if(media >=4){
+    print("Você está de recuperação");
+  }else{
+    print("Você está reprovado!");
+  }
+}
